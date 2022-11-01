d2354e27c679df409a18275c05e14b8685c423a9
Gabriel Gomes <gabriel.gomes@rethink.dev>
2022-11-01 09:53:27 -0300

    starting

--- src/ProductList/machine.ts
+++ src/ProductList/machine.ts
@@ -1,9 +1,59 @@
 // PEGAR DATA E MANDAR PARA O SERVICE
-import { ORDERS, ORDERS_FINAL_RESPONSE } from '../data/mockOrders';
+import { ORDERS } from '../data/mockOrders';
 
 const productListMachine = () => {
-  const newData = ORDERS.orderList[0];
-  return ORDERS_FINAL_RESPONSE;
+  const library = {
+    feeType: {
+      condition: ["fee", "type"],
+      types: {
+        RESERVATION: "Reserva de bilhetes",
+        NO_MILES_BOOKING: "Viaje Fácil",
+        BOARDING: "Voo",
+        CONVENIENCE: "Taxas de conveniência",
+        CANCELLATION: "Cancelamento de Voo",
+      },
+    },
+    milesOperation: {
+      condition: ["miles", "operation"],
+      types: {
+        PURCHASE: "Compra de milhas",
+        REVALIDATION: "Reativação de milhas",
+        TRANSFER: "Transferência de milhas",
+        EXTENSION: "Produto Extensão de Milhas",
+        EXTEND: "Produto Extensão de Milhas",
+      },
+    },
+    productType: {
+      condition: ["product", "type"],
+      types: {
+        TRAVEL_INSURANCE: "Seguro Viagem",
+        UBER_CREDIT: "Compra de créditos Uber",
+        UBER_PASS: "Assinatura Uber Pass",
+        LOCALIZA: "Localiza/Hertz",
+      },
+    },
+    cancelProductType: {
+      condition: ["cancelProduct", "type"],
+      type: {
+        TRAVEL_INSURANCE: "Cancelamento de seguro viagem",
+        UBER_CREDIT: "Cancelamento Voucher Uber",
+        UBER_PASS: "Cancelamento Assinatura Voucher Uber",
+        LOCALIZA: "Cancelamento de Localiza/Hertz",
+      },
+    },
+    booking: {
+      notNull: true,
+      type: {
+        "Bilhetes": ["booking", "flight"],
+        "Assento": ["booking", "seat"],
+        "Bagagem": ["booking", "baggage"],
+        "Cancelamento de bagagem": ["cancelBooking", "baggage"],
+        "Clube Smiles": ["membership"],
+      }
+    },
+  };
+  const data = ORDERS;
+  return data;
 };
 
 export default { productListMachine };
--- test/productList.test.ts
+++ test/productList.test.ts
@@ -3,13 +3,12 @@ import request from 'supertest';
 
 import { ORDERS_FINAL_RESPONSE } from '../src/data/mockOrders';
 import app from '../src/listen';
-import Machine from '../src/ProductList/machine';
 
 describe("Product List", () => {
   test("Expect test to be exatly equals to mock", async () => {
-    jest
-      .spyOn(Machine, "productListMachine")
-      .mockReturnValue(JSON.stringify(ORDERS_FINAL_RESPONSE));
+    // jest
+    //   .spyOn(Machine, "productListMachine")
+    //   .mockReturnValue(JSON.stringify(ORDERS_FINAL_RESPONSE));
 
     const response = await request(app).get("/api/productList");
 
